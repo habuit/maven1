471e130609817c0eb3a49bbb0f0738b751626112
habuit <125548294+habuit@users.noreply.github.com>
2024-03-06 11:46:49 +0200

    Update AppTest.java

--- src/test/java/ToolsQA/AppTest.java
+++ src/test/java/ToolsQA/AppTest.java
@@ -1,6 +1,39 @@
-class HelloWorld {
-// Your program begins with a call to main().
-// Prints "Hello, World" to the terminal window.
-public static void main(String args[])
+package ToolsQA;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
 {
-System.out.println("Hello, Maven");
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+
